c423599263c9f851f0f6aded6d754a57ce9cf740
fahmifan <fahmifan909@gmail.com>
2018-04-28 14:29:50 +0700

    add NewAksaraAdapter

---
+++ app/src/main/java/com/example/mortezasaadat/animalquiz/NewAksaraAdapter.java
@@ -0,0 +1,15 @@
+Unstaged changes after reset:
+M	.idea/caches/build_file_checksums.ser
+M	.idea/codeStyles/Project.xml
+M	.idea/misc.xml
+M	.idea/modules.xml
+M	app/src/main/AndroidManifest.xml
+D	app/src/main/java/com/example/mortezasaadat/animalquiz/AksaraAdapter.java
+M	app/src/main/java/com/example/mortezasaadat/animalquiz/AksaraBakuCategoryPagerAdapter.java
+M	app/src/main/java/com/example/mortezasaadat/animalquiz/MainActivity.java
+M	app/src/main/java/com/example/mortezasaadat/animalquiz/NewAksaraAdapter.java
+M	app/src/main/java/com/example/mortezasaadat/animalquiz/NewAksaraFragment.java
+M	app/src/main/res/layout/activity_main.xml
+M	app/src/main/res/values/arrays.xml
+M	app/src/main/res/values/strings.xml
+M	build.gradle
