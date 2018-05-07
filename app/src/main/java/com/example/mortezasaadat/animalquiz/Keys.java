1751b8a70b18890aa835380358e00acc555232b8
fahmifan <fahmifan909@gmail.com>
2018-05-07 17:35:26 +0700

    add new Keys

--- app/src/main/java/com/example/mortezasaadat/animalquiz/Keys.java
+++ app/src/main/java/com/example/mortezasaadat/animalquiz/Keys.java
@@ -4,4 +4,5 @@ public final class Keys {
     public static final String AKSARA_TYPE = "AKSARA_TYPE";
     public static final String AKSARA_BAKU = "AKSARA_BAKU";
     public static final String AKSARA_KUNO = "AKSARA_KUNO";
+    public static final String AKSARA_KUNO_PRASASTI = "AKSARA_KUNO_PRASASTI";
 }
